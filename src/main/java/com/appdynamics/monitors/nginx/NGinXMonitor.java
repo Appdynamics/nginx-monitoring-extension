4094d4f3954d65a2e3447efcd9aac0ff3d3225d3
Satish Reddy M <satish.m@imaginea.com>
2014-11-03 19:00:33 +0530

    Changes to support NGinX Plus.

--- src/main/java/com/appdynamics/monitors/nginx/NGinXMonitor.java
+++ src/main/java/com/appdynamics/monitors/nginx/NGinXMonitor.java
@@ -1,4 +1,38 @@
+/**
+ * Copyright 2014 AppDynamics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.appdynamics.monitors.nginx;
+
+import com.appdynamics.extensions.ArgumentsValidator;
+import com.appdynamics.extensions.http.Response;
+import com.appdynamics.extensions.http.SimpleHttpClient;
+import com.appdynamics.extensions.http.UrlBuilder;
+import com.appdynamics.monitors.nginx.statsExtractor.CachesStatsExtractor;
+import com.appdynamics.monitors.nginx.statsExtractor.ConnectionsStatsExtractor;
+import com.appdynamics.monitors.nginx.statsExtractor.RequestsStatsExtractor;
+import com.appdynamics.monitors.nginx.statsExtractor.ServerZoneStatsExtractor;
+import com.appdynamics.monitors.nginx.statsExtractor.StatsExtractor;
+import com.appdynamics.monitors.nginx.statsExtractor.UpstreamsStatsExtractor;
+import com.singularity.ee.agent.systemagent.api.AManagedMonitor;
+import com.singularity.ee.agent.systemagent.api.MetricWriter;
+import com.singularity.ee.agent.systemagent.api.TaskExecutionContext;
+import com.singularity.ee.agent.systemagent.api.TaskOutput;
+import com.singularity.ee.agent.systemagent.api.exception.TaskExecutionException;
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,197 +42,183 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.log4j.Logger;
-
-import com.singularity.ee.agent.systemagent.api.AManagedMonitor;
-import com.singularity.ee.agent.systemagent.api.MetricWriter;
-import com.singularity.ee.agent.systemagent.api.TaskExecutionContext;
-import com.singularity.ee.agent.systemagent.api.TaskOutput;
-import com.singularity.ee.agent.systemagent.api.exception.TaskExecutionException;
-import com.singularity.ee.util.httpclient.HttpClientWrapper;
-import com.singularity.ee.util.httpclient.HttpExecutionRequest;
-import com.singularity.ee.util.httpclient.HttpExecutionResponse;
-import com.singularity.ee.util.httpclient.HttpOperation;
-import com.singularity.ee.util.httpclient.IHttpClientWrapper;
-import com.singularity.ee.util.log4j.Log4JLogger;
 
 /**
- * NGinXStatusMonitor is a class that provides metrics on NGinX server by using the 
+ * NGinXStatusMonitor is a class that provides metrics on NGinX server by using the
  * NGinX status stub.
  */
-public class NGinXMonitor extends AManagedMonitor
-{
-	/**
-	 * The metric can be found in Application Infrastructure Performance|{@literal <}Node{@literal >}|Custom Metrics|WebServer|NGinX|Status
-	 */
-	private final static String metricPrefix = "Custom Metrics|WebServer|NGinX|Status|";
+public class NGinXMonitor extends AManagedMonitor {
+    /**
+     * The metric can be found in Application Infrastructure Performance|{@literal <}Node{@literal >}|Custom Metrics|WebServer|NGinX|Status
+     */
+    private static final Logger logger = Logger.getLogger(NGinXMonitor.class);
 
-	protected volatile String host;
-	protected volatile String port;
-	protected volatile String location;
+    private static final String METRIC_SEPARATOR = "|";
 
-	private Map<String,String> resultMap = new HashMap<String,String>();
+    private static final Map<String, String> DEFAULT_ARGS = new HashMap<String, String>() {{
+        put("host", "localhost");
+        put("port", "8080");
+        put("location", "nginx_status");
+        put("metric-prefix", "Custom Metrics|WebServer|NGinX");
+    }};
 
-	protected final Logger logger = Logger.getLogger(this.getClass().getName());
+    public NGinXMonitor() {
+        String version = getClass().getPackage().getImplementationTitle();
+        String msg = String.format("Using Monitor Version [%s]", version);
+        logger.info(msg);
+        System.out.println(msg);
+    }
 
-	/**
-	 * Main execution method that uploads the metrics to the AppDynamics Controller
-	 * @see com.singularity.ee.agent.systemagent.api.ITask#execute(java.util.Map, com.singularity.ee.agent.systemagent.api.TaskExecutionContext)
-	 */
-	public TaskOutput execute(Map<String, String> arg0, TaskExecutionContext arg1)
-			throws TaskExecutionException
-	{
-		try 
-		{
-			host = arg0.get("host");
-			port = arg0.get("port");
-			location = arg0.get("location");
+    /**
+     * Main execution method that uploads the metrics to the AppDynamics Controller
+     *
+     * @see com.singularity.ee.agent.systemagent.api.ITask#execute(java.util.Map, com.singularity.ee.agent.systemagent.api.TaskExecutionContext)
+     */
+    public TaskOutput execute(Map<String, String> argsMap, TaskExecutionContext executionContext)
+            throws TaskExecutionException {
+        try {
 
-			populate();
+            logger.debug("The args map before filling the default is:  " + argsMap);
+            argsMap = ArgumentsValidator.validateArguments(argsMap, DEFAULT_ARGS);
+            logger.debug("The args map after filling the default is: " + argsMap);
 
-			printMetric("Activity|up", 1,
-				MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION, 
-				MetricWriter.METRIC_TIME_ROLLUP_TYPE_SUM,
-				MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE);
+            Map<String, String> resultMap = populate(argsMap);
 
-			printMetric("Activity|Active Connections", resultMap.get("ACTIVE_CONNECTIONS"),
-				MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
-				MetricWriter.METRIC_TIME_ROLLUP_TYPE_CURRENT,
-				MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_INDIVIDUAL
-			);
+            String metricPrefix = argsMap.get("metric-prefix");
 
-			printMetric("Activity|Server|Accepts", resultMap.get("SERVER_ACCEPTS"),
-				MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
-				MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
-				MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-			);
+            printAllMetrics(metricPrefix, resultMap);
 
-			printMetric("Activity|Server|Handled", resultMap.get("SERVER_HANDLED"),
-				MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
-				MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
-				MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-			);
+            logger.info("NGinX Metric Upload Complete");
+            return new TaskOutput("NGinX Metric Upload Complete");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new TaskOutput("Error: " + e);
+        }
+    }
 
-			printMetric("Activity|Server|Requests", resultMap.get("SERVER_REQUESTS"),
-				MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
-				MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
-				MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-			);
+    /**
+     * Fetches Statistics from NGinX Server
+     *
+     * @param argsMap arguments passed
+     */
+    private Map<String, String> populate(Map<String, String> argsMap) throws IOException, TaskExecutionException {
 
-			printMetric("Activity|Reading", resultMap.get("READING"),
-				MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
-				MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
-				MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-			);
+        SimpleHttpClient httpClient = SimpleHttpClient.builder(argsMap).build();
+        String url = UrlBuilder.builder(argsMap).path(argsMap.get("location")).build();
+        Response response = httpClient.target(url).get();
+        String responseBody = response.string();
+        String header = response.getHeader("Content-Type");
 
-			printMetric("Activity|Writing", resultMap.get("WRITING"),
-				MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
-				MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
-				MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-			);
+        Map<String, String> resultMap = null;
+        if ("application/json".equals(header)) {
+            resultMap = parsePlusStatsResult(responseBody);
+        } else if ("text/plain".equals(header)) {
+            resultMap = parseStubStatsResults(responseBody);
+        } else {
+            logger.error("Invalid content type for URL " + url);
+            throw new TaskExecutionException("Invalid content type for URL " + url);
+        }
+        return resultMap;
+    }
 
-			printMetric("Activity|Waiting", resultMap.get("WAITING"),
-				MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
-				MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
-				MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
-			);
-
-			return new TaskOutput("NGinX Metric Upload Complete");
-		}
-		catch (Exception e)
-		{
-			return new TaskOutput("Error: " + e);
-		}
-	}
-
-	/**
-	 * Fetches Statistics from NGinX Server
-	 * @throws InstantiationException
-	 * @throws IllegalAccessException
-	 * @throws ClassNotFoundException
-	 * @throws IOException
-	 */
-	protected void populate() throws InstantiationException,
-			IllegalAccessException, ClassNotFoundException, IOException
-	{
-		IHttpClientWrapper httpClient = HttpClientWrapper.getInstance();
-
-		HttpExecutionRequest request = new HttpExecutionRequest(getConnectionURL(), "", HttpOperation.GET);
-		HttpExecutionResponse response = httpClient.executeHttpOperation(request, new Log4JLogger(logger));
+    private Map<String, String> parsePlusStatsResult(String responseBody) {
+        Map<String, String> resultMap = new HashMap<String, String>();
+        JSONObject jsonObject = new JSONObject(responseBody);
 
 
-	    parseResults(response.getResponseBody());
-	}
+        StatsExtractor connectionsStatsExtractor = new ConnectionsStatsExtractor();
+        Map<String, String> connectionStats = connectionsStatsExtractor.extractStats(jsonObject);
+        resultMap.putAll(connectionStats);
 
-    public void parseResults(String responseBody) throws IOException {
+        StatsExtractor requestsStatsExtractor = new RequestsStatsExtractor();
+        Map<String, String> requestStats = requestsStatsExtractor.extractStats(jsonObject);
+        resultMap.putAll(requestStats);
+
+        StatsExtractor serverZoneStatsExtractor = new ServerZoneStatsExtractor();
+        Map<String, String> serverZonesStats = serverZoneStatsExtractor.extractStats(jsonObject);
+        resultMap.putAll(serverZonesStats);
+
+        StatsExtractor upstreamsStatsExtractor = new UpstreamsStatsExtractor();
+        Map<String, String> upstreamsStats = upstreamsStatsExtractor.extractStats(jsonObject);
+        resultMap.putAll(upstreamsStats);
+
+        StatsExtractor cachesStatsExtractor = new CachesStatsExtractor();
+        Map<String, String> cachesStats = cachesStatsExtractor.extractStats(jsonObject);
+        resultMap.putAll(cachesStats);
+
+        return resultMap;
+    }
+
+
+    private Map<String, String> parseStubStatsResults(String responseBody) throws IOException {
+
+        Map<String, String> resultMap = new HashMap<String, String>();
+
         Pattern numPattern = Pattern.compile("\\d+");
         Matcher numMatcher;
 
         BufferedReader reader = new BufferedReader(new StringReader(responseBody));
         String line, whiteSpaceRegex = "\\s";
 
-        while ((line=reader.readLine()) != null)
-        {
-            if (line.contains("Active connections"))
-            {
+        while ((line = reader.readLine()) != null) {
+            if (line.contains("Active connections")) {
                 numMatcher = numPattern.matcher(line);
                 numMatcher.find();
-                resultMap.put("ACTIVE_CONNECTIONS", numMatcher.group());
-            }
-            else if (line.contains("server"))
-            {
+                resultMap.put("Active Connections", numMatcher.group());
+            } else if (line.contains("server")) {
                 line = reader.readLine();
 
                 String[] results = line.trim().split(whiteSpaceRegex);
 
-                resultMap.put("SERVER_ACCEPTS", results[0]);
-                resultMap.put("SERVER_HANDLED", results[1]);
-                resultMap.put("SERVER_REQUESTS", results[2]);
-            }
-            else if (line.contains("Reading"))
-            {
+                resultMap.put("Server|Accepts", results[0]);
+                resultMap.put("Server|Handled", results[1]);
+                resultMap.put("Server|Requests", results[2]);
+            } else if (line.contains("Reading")) {
                 String[] results = line.trim().split(whiteSpaceRegex);
-                resultMap.put("READING", results[1]);
-                resultMap.put("WRITING", results[3]);
-                resultMap.put("WAITING", results[5]);
+                resultMap.put("Reading", results[1]);
+                resultMap.put("Writing", results[3]);
+                resultMap.put("Waiting", results[5]);
             }
         }
+        return resultMap;
     }
 
     /**
-	 * Returns the metric to the AppDynamics Controller.
-	 * @param 	metricName		Name of the Metric
-	 * @param 	metricValue		Value of the Metric
-	 * @param 	aggregation		Average OR Observation OR Sum
-	 * @param 	timeRollup		Average OR Current OR Sum
-	 * @param 	cluster			Collective OR Individual
-	 */
-	public void printMetric(String metricName, Object metricValue, String aggregation, String timeRollup, String cluster)
-	{
-		MetricWriter metricWriter = getMetricWriter(getMetricPrefix() + metricName, 
-			aggregation,
-			timeRollup,
-			cluster
-		);
+     * @param metricPrefix
+     * @param resultMap
+     */
+    private void printAllMetrics(String metricPrefix, Map<String, String> resultMap) {
+        for (Map.Entry<String, String> metricEntry : resultMap.entrySet()) {
+            printMetric(metricPrefix, metricEntry.getKey(), metricEntry.getValue());
+        }
 
-		metricWriter.printMetric(String.valueOf(metricValue));
-	}
+    }
 
-	protected String getConnectionURL()
-	{
-		return "http://" + host + ":" + port + "/" + location;
-	}
+    /**
+     * Returns the metric to the AppDynamics Controller.
+     *
+     * @param metricPrefix Metric prefix
+     * @param metricName   Name of the Metric
+     * @param metricValue  Value of the Metric
+     */
+    private void printMetric(String metricPrefix, String metricName, Object metricValue) {
+        System.out.println(metricPrefix + METRIC_SEPARATOR + metricName+":::"+metricValue);
+        MetricWriter metricWriter = getMetricWriter(metricPrefix + METRIC_SEPARATOR + metricName,
+                MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
+                MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE
+        );
 
-	protected String getMetricPrefix()
-	{
-		return metricPrefix;
-	}
+        metricWriter.printMetric(String.valueOf(metricValue));
+    }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         NGinXMonitor nGinXMonitor = new NGinXMonitor();
         try {
-            nGinXMonitor.execute(new HashMap<String, String>(),null);
+            nGinXMonitor.execute(new HashMap<String, String>(), null);
         } catch (TaskExecutionException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
---
+++ src/main/java/com/appdynamics/monitors/nginx/statsExtractor/UpstreamsStatsExtractor.java
@@ -0,0 +1,137 @@
+/**
+ * Copyright 2014 AppDynamics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.appdynamics.monitors.nginx.statsExtractor;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+public class UpstreamsStatsExtractor implements StatsExtractor {
+
+    @Override
+    public Map<String, String> extractStats(JSONObject respJson) {
+        JSONObject upstreams = respJson.getJSONObject("upstreams");
+        Map<String, String> upstreamsStats = getUpstreamsStats(upstreams);
+        return upstreamsStats;
+    }
+
+    private Map<String, String> getUpstreamsStats(JSONObject upstreams) {
+        Map<String, String> upstreamsStats = new HashMap<String, String>();
+        Set<String> serverGroupNames = upstreams.keySet();
+
+        for (String serverGroupName : serverGroupNames) {
+            JSONArray serverGroups = upstreams.getJSONArray(serverGroupName);
+
+            for (int i = 0; i < serverGroups.length(); i++) {
+                JSONObject server = serverGroups.getJSONObject(i);
+
+                String serverIp = server.getString("server");
+
+                boolean backup = server.getBoolean("backup");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|backup", backup ? "1" : "0");
+
+                long weight = server.getLong("weight");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|weight", String.valueOf(weight));
+
+                // “up”, “down”, “unavail”, or “unhealthy”.
+                String state = server.getString("state");
+                int stateInt = -1;
+                if ("up".equals(state)) {
+                    stateInt = 0;
+                } else if ("down".equals(state)) {
+                    stateInt = 1;
+                } else if ("unavail".equals(state)) {
+                    stateInt = 2;
+                } else if ("unhealthy".equals(state)) {
+                    stateInt = 3;
+                }
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|state", String.valueOf(stateInt));
+
+                long active = server.getLong("active");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|active", String.valueOf(active));
+
+                long keepalive = server.getLong("keepalive");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|keepalive", String.valueOf(keepalive));
+
+                if (server.has("max_conns")) {
+                    long maxConns = server.getLong("max_conns");
+                    upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|max_conns", String.valueOf(maxConns));
+                }
+
+                long requests = server.getLong("requests");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|requests", String.valueOf(requests));
+
+                JSONObject responses = server.getJSONObject("responses");
+                long resp1xx = responses.getLong("1xx");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|responses|1xx", String.valueOf(resp1xx));
+
+                long resp2xx = responses.getLong("2xx");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|responses|2xx", String.valueOf(resp2xx));
+
+                long resp3xx = responses.getLong("3xx");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|responses|3xx", String.valueOf(resp3xx));
+
+                long resp4xx = responses.getLong("4xx");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|responses|4xx", String.valueOf(resp4xx));
+
+                long resp5xx = responses.getLong("5xx");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|responses|5xx", String.valueOf(resp5xx));
+
+                long respTotal = responses.getLong("total");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|responses|total", String.valueOf(respTotal));
+
+                long sent = server.getLong("sent");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|sent", String.valueOf(sent));
+
+                long received = server.getLong("received");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|received", String.valueOf(received));
+
+                long upstreamServerFails = server.getLong("fails");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|fails", String.valueOf(upstreamServerFails));
+
+                long unavail = server.getLong("unavail");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|unavail", String.valueOf(unavail));
+
+                JSONObject healthChecks = server.getJSONObject("health_checks");
+                long checks = healthChecks.getLong("checks");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|health_checks|checks", String.valueOf(checks));
+
+                long healthCheckFails = healthChecks.getLong("fails");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|health_checks|fails", String.valueOf(healthCheckFails));
+
+                long unhealthy = healthChecks.getLong("unhealthy");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|health_checks|unhealthy", String.valueOf(unhealthy));
+
+                if (server.has("last_passed")) {
+                    boolean lastPassed = healthChecks.getBoolean("last_passed");
+                    upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|health_checks|last_passed", String.valueOf(lastPassed ? 0 : 1));
+                }
+
+                long downtime = server.getLong("downtime");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|downtime", String.valueOf(downtime));
+
+                long downstart = server.getLong("downstart");
+                upstreamsStats.put("upstreams|" + serverGroupName + " |" + serverIp + "|downstart", String.valueOf(downstart));
+            }
+        }
+        return upstreamsStats;
+    }
+}
